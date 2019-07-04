8915654dd5384ac93d73f8eda2bd65ea145dcb7c
Le Huyen Tran <lehuyentran@Les-MacBook-Pro.local>
2019-07-04 22:45:06 +0300

    finish menu

--- components/Header.js
+++ components/Header.js
@@ -1,35 +1,56 @@
 import Link from 'next/link'
 import Img from './Logo'
 
-const linkStyle = {
-  marginRight: 15
-  
-}
 
 export default function Header() {
   return (
     <div>
-      <Img />
-      <Link href="/">
-        <a style={linkStyle}>Home</a>
-      </Link>
-      <Link href="/games">
-        <a style={linkStyle}>Games</a>
-      </Link>
-      <Link href="/team">
-        <a style={linkStyle}>Team</a>
-      </Link>
-      <Link href="/contact">
-        <a style={linkStyle}>Contact</a>
-      </Link>
-      <style jsx>{`
-         a {
-          color: red;
-          font-family: Orbitron;
-          font-size:25px;
-        }
-      `}</style>
+      <div className="header">
+        <div className="nav_logo">
+          <Img />
+        </div>
 
+        <div className="menu">
+          <Link href="/">
+            <a className="link">HOME</a>
+          </Link>
+          <Link href="/games">
+            <a className="link">GAMES</a>
+          </Link>
+          <Link href="/team">
+            <a className="link">TEAM</a>
+          </Link>
+          <Link href="/contact">
+            <a className="link">CONTACT</a>
+          </Link>
+        </div>
+      </div>
+      <style jsx>{`
+      .nav_logo {
+        padding: 10px 30px;
+        width: 60px;
+        height: 60px;
+      }
+      .menu {  
+        padding-right: 30px;
+          }
+
+      .link {
+        color: #444141;
+        margin-right: 20px;
+        font-family: Orbitron;
+        font-size:25px;
+      }
+
+      .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #F9A825;
+        height: 80px;
+      }
+    `}</style>
     </div>
+
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,19 @@ export default function Blog() {
     <Layout>
       <Img />
       <h1 className="title">LIFT STUDIO</h1>
-      <p>Ut enim ad minim veniam,laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p className="description">Ut enim ad minim veniam,laboris nisi ut aliquip ex ea commodo consequat.</p>
+      
       <style jsx>
         {`
         .title {
           font-family: 'Orbitron';
           font-size:45px;
-        }`
         }
+        .description {
+          font-family: 'Montserrat';
+          font-size: 20px;
+        }
+        `}
       </style>
     </Layout>
   );

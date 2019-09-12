fc332065631319e91c75c873ad79e736210fd3ef
haduyhien121192@gmail.com <anhvudao17@gmail.com>
2019-09-12 23:28:29 +0300

    Deploy gh-pages

--- components/ResponsiveJS/ResponsiveLayout/index.js
+++
@@ -1,6 +0,0 @@
-import { useWindowDimensions } from '../WindowDimensionsProvider'
-
-export default function ResponsiveLayout ( {breakPoint, renderDesktop, renderMobile} ) {
-    const { width } = useWindowDimensions()
-    return width > breakPoint ? renderDesktop() : renderMobile()
-  }

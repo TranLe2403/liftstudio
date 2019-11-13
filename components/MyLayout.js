cce08953e51c80bd907d940de6616801660173f8
Le Huyen Tran <lehuyentran@Les-MacBook-Pro.local>
2019-11-13 15:22:00 +0200

    update

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -5,21 +5,19 @@ import Header from './Header'
 export default function Layout(props) {
   return (
     <div>
-    <div className="nav">
-      <Header />
-      {props.children}
+      <div className="nav">
+        <Header />
+          {props.children}
+      </div>
+
+      <style jsx>{`
+        .nav{
+          height: 100vh;
+          display: flex;
+          flex-direction: column;
+        }  
+      `}
+      </style>
     </div>
-
-    <style jsx>{`
-      .nav{
-        height: 100vh;
-        display: flex;
-        flex-direction: column;
-
-
-      }  
-  `}
-  </style>
-  </div>
   )
 }

6b9c70789147881b02843e4399319d8feb86e8b0
Quan Dao <daq30899@gmail.com>
2020-03-11 15:53:48 +0200

    delete static folder, adding netlifycms to project

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   // some configuration
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/SumWebPro' : '',
+  assetPrefix: process.env.NODE_ENV === "production" ? "/SumWebPro" : "",
   // another configuration
-}
+  webpack: cfg => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: "frontmatter-markdown-loader",
+      options: { mode: ["react-component"] }
+    });
+    return cfg;
+  }
+};

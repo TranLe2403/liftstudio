8b7aea143bdbdbf0675bcf9061dd740111ce4acb
Quan Dao <daq30899@gmail.com>
2020-03-20 12:15:50 +0200

    changed css on team to using styled components

--- pages/team.js
+++ pages/team.js
@@ -1,17 +1,72 @@
 import Layout from "../components/MyLayout.js";
 import Person from "../components/Person.js";
 import { attributes } from "../content/team.md";
+import styled from "styled-components";
+
+const Container = styled.div`
+  flex-grow: 1;
+`;
+
+const TitleBar = styled.div`
+  height: 65px;
+  background-color: #f9a825;
+  text-align: center;
+  flex-grow: 1;
+
+  @media (min-width: 768px) {
+    height: 100px;
+  }
+`;
+
+const Title = styled.h1`
+  margin-top: 20px;
+  font-family: "Orbitron";
+  text-align: center;
+  font-size: 26px;
+  border-bottom: 3px solid #444141;
+  letter-spacing: 5px;
+  color: #444141;
+  display: inline-block;
+
+  @media (min-width: 768px) {
+    font-size: 60px;
+    margin-top: 0px;
+  }
+`;
+
+const Background = styled.div`
+  background-color: #c4c4c4;
+  opacity: 0.85;
+  height: 100%;
+`;
+
+const TeamContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  justify-content: space-around;
+  background-color: #040404;
+  opacity: 0.85;
+  height: 100%;
+  flex-wrap: wrap;
+  padding: 20px;
+  box-sizing: border-box;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
 
 export default function Team(props) {
   let { title, persons } = attributes;
   return (
     <Layout>
-      <div className="team_page">
-        <div className="title_bar">
-          <h1 className="title">{title}</h1>
-        </div>
-        <div className="bg">
-          <div className="team">
+      <Container>
+        <TitleBar>
+          <Title className="title">{title}</Title>
+        </TitleBar>
+        <Background className="bg">
+          <TeamContainer className="team">
             {persons.map(person => {
               return (
                 <Person
@@ -25,70 +80,9 @@ export default function Team(props) {
                 />
               );
             })}
-          </div>
-        </div>
-      </div>
-
-      <style jsx>
-        {`
-          .team_page {
-            flex-grow: 1;
-          }
-
-          .title_bar {
-            height: 65px;
-            background-color: #f9a825;
-            text-align: center;
-            flex-grow: 1;
-          }
-          .title {
-            margin-top: 20px;
-            font-family: "Orbitron";
-            text-align: center;
-            font-size: 26px;
-            border-bottom: 3px solid #444141;
-            letter-spacing: 5px;
-            color: #444141;
-            display: inline-block;
-          }
-          .team {
-            display: flex;
-            flex-direction: column;
-            align-items: stretch;
-            justify-content: space-around;
-            background-color: #040404;
-            opacity: 0.85;
-            height: 100%;
-            flex-wrap: wrap;
-            padding: 20px;
-            box-sizing: border-box;
-          }
-          .bg {
-            background-color: #c4c4c4;
-            opacity: 0.85;
-            height: 100%;
-          }
-          @media screen and (min-width: 768px) {
-            /* For desktop: */
-            .title_bar {
-              height: 100px;
-            }
-            .title {
-              font-size: 60px;
-              margin-top: 0px;
-            }
-            .team {
-              flex-direction: row;
-              height: 100%;
-              align-items: stretch;
-              justify-content: space-around;
-            }
-            .bg {
-              height: 100%;
-            }
-          }
-        `}
-      </style>
+          </TeamContainer>
+        </Background>
+      </Container>
     </Layout>
   );
 }
